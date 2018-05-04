5e0c166c50b38c4938858a8e08a1ab3025b2c897
tanpham <tester@Mac-mini-cua-tester.local>
2018-05-04 17:28:19 +0700

    get and set

--- hashtable.py
+++ hashtable.py
@@ -17,8 +17,17 @@
 """
 
 
-def hash():
-	pass
+def hash(key_str, size):
+	return sum([ord(c) for c in key_str]) % size
+
+
+class HashEntry(object):
+
+	def __init__(self, key, value):
+		self.key = key
+		self.value = value		
+		self.next = None # final item in linkedlist point to None
+
 
 class HashTable(object):
 	
@@ -29,19 +38,65 @@ class HashTable(object):
 		self.current_capacity = 0
 		self.slots = [None] * capacity
 
+
 	def set(self, key, value):
-		pass
+		key_hashed = hash(key, self.capacity)
+
+		if self.slots[key_hashed] is None:
+			self.slots[key_hashed] = HashEntry(key, value)
+		else:
+			self._add_to_linked_list(key, value, self.slots, key_hashed)
+
+
+	# check if key is same value
+	def _add_to_linked_list(self, key, value, slots, key_hashed):
+		# same key found --> update value with new value
+		# difference key, and current node is final node in linked list --> add new node to list
+		# if current node is not final node --> continue moving on linked list
+		current_node = slots[key_hashed]
+
+		while current_node is not None:
+
+			# update value if same key found
+			if current_node.key == key:
+				print('update happen')
+				current_node.value = value
+				current_node = None
+			else:
+				# this is final node
+				if current_node.next == None:
+					current_node.next = HashEntry(key, value)
+				# this is not final node
+				# continue to move
+				else:
+					current_node = current_node.next
+
 
 	def get(self,key):
-		pass
+		key_hashed = hash(key, self.capacity)
+		
+		node = self.slots[key_hashed]
+
+		while node is not None:
+			if node.key == key:
+				return node.value
+			else:
+				node = node.next
 
 
-class HashEntry(object):
-	def __init__(self, key, value):
-		self.key = key
-		self.value = value
-		# final item in linkedlist point to None
-		self.next = None
+ht = HashTable(1000,500)
+ht.set('hello', 4)
+print(ht.get('hello'))
+ht.set('hello', 5)
+print(ht.get('hello'))
+ht.set('abc',10)
+print(ht.get('abc'))
+
+
+
+
+
+
 
 		
 
